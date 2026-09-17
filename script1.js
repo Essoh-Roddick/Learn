d4214ae101bbf56523dde7a86772a6088926f2e6
ESSOH-RODDICK <roddick388@gmail.com>
2026-09-17 14:35:54 +0100

    Made some changes

--- script1.js
+++ script1.js
@@ -75,6 +75,7 @@ switch (message) {
 console.log(mult(3,5));
 */
 
+/*
 // setTimeot  and setInterval Test
  let time = document.getElementById("time");
  let tm = document.getElementById("tm");
@@ -103,4 +104,100 @@ displ();
     setInterval(displ,1000);
     console.log("ooh")
  }
-setI(); 
+setI();  */
+
+// JS objects
+
+const obj1 = document.getElementById("obj1");
+const obj2 = document.getElementById("obj2");
+const obj3 = document.getElementById("obj3");
+
+const  car ={
+    type: "Fiat",
+    model: "500",
+    color: "white",
+   // object nested in another object
+    owner: {
+    fName: "Josh",
+    lName: "Jobs"
+   } 
+};
+
+obj1.innerHTML = "Type:" + " " + car.type + "<br>" 
+ + "Model:" + " " + car["model"] + "<br>"
+ + "Color:" + " " + car.color + "<br>" 
+ + "Owner's name:" + " " + car.owner.fName + " " + car.owner.lName ;
+
+
+const person ={
+    fName: "John",
+    lName: "Doe",
+    age: 25,
+    tbd: "to be deleted",
+    fullName: function(){
+        return this.fName + " " + this.lName;
+    }
+};
+
+
+// changing the value of fName afterwards
+person.fName = "dick";  
+// Creating a new property afterwards
+person.lang = "English";
+// Creating a new method afterwards
+person.ageInYears = function(){
+    return  (this.age + " years").toUpperCase();
+};
+// deleting the tbd property
+delete person.tbd;
+// Using in to check if a property exists in the object
+let check = ("tbd" in person); 
+
+obj2.innerHTML = "Name: " + " " + person.fullName()
+ + "<br>" + "Age: " + " " + person.ageInYears() 
+ + "<br>" + "Language: " + " " + person.lang
+ + "<br>" + "tbd: " + " " + person.tbd
+ + "<br>" + "Check: " + " " + check;  // will return undefined if tbd was deleted
+const fruits = {Bananas:300, Oranges:200, Apples:500}; 
+
+// js Construtor function
+function Person(fN, lN, age, height){
+    this.fName = fN;
+    this.lName = lN;
+    this.age = age;
+    this.height = height;
+    this.lang = "English";
+}
+
+// Create new person object
+const Man = new Person("Jonas", "Jacob", "26", "1.7");
+
+ // Displaying objects
+
+let text = "";
+
+text = "Name: " + " " + Man.fName + " " + Man.lName 
+ + "<br>" + "Age: " + " " + Man.age
+ + "<br>" + "Height: " + " " + Man.height
+ + "<br>" + "Language: " + " " + Man.lang;
+
+//Using JSON.stringify() to display the object
+ //text = JSON.stringify(fruits);
+
+ /*
+//Using Object.entries() to loop through the object
+for (let [fruit, amount] of Object.entries(fruits)) {
+  text += fruit + ": " + amount + "<br>";
+}           */
+
+/*
+//Using an array
+const arr = Object.values(person);
+text = arr.toString();  */
+
+/*
+// Using a for loop
+for (let x in person){
+    text += x + ": " + person[x] + "<br>";
+}   */
+  obj3.innerHTML = text
